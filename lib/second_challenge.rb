4192c31af9b03b5a460b75b696b9a6900fe76c3f
Preston Elliott <preston.elliott24@gmail.com>
2019-04-11 17:31:18 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,8 +7,8 @@ def second_challenge
   }
 
   #code your solution here!
-  groceries.each do |key, value|
-    value.values.each do |k, v|
+  groceries.values.each do |key, value|
+    value.each do |k, v|
     puts "#{v}"
     end
   end
